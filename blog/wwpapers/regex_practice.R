49d25cdc810c2e3c81e3c550e3b373f9b5620088
Emma Holt <81181898+emmaholt833@users.noreply.github.com>
2023-02-14 12:10:51 -0700

    add emma's files

---
+++ blog/wwpapers/regex_practice.R
@@ -0,0 +1,8 @@
+
+# global mode finds all instead of first instance
+# GREP: global regular expression print
+# use "\" to escape any metacharacter - even itself
+
+# use "[]" for custom set of values - interchangable
+
+
